d29b072339993e26c8256f7c59ffc8a9d5f5c3f6
Tillie Slosser <tillie@slosser.net>
2024-04-13 21:44:59 -0400

    final submit

---
+++ Player.java
@@ -0,0 +1,332 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Player implements Contract {
+    // Attributes
+    protected ArrayList<String> inventory;
+    protected Place currentPlace;
+    protected ArrayList<Place> map; // map of complete game
+    private int playerSize; // the size of the player for pulling some Alice-in-Wonderland-type magic tricks
+    private int playerEnergy; // The energy level of the player
+    private List<String> actions;
+    private List<String> actionArgs;
+
+    public Player(Place currentPlace, ArrayList<Place> map) {
+        this.currentPlace = currentPlace;
+        this.map = map;
+        this.inventory = new ArrayList<String>();
+        this.playerSize = 3;
+        this.playerEnergy = 100;
+        this.actions = new ArrayList<String>();
+        this.actionArgs = new ArrayList<String>();
+        System.out.println("You have made a Player!");
+    }
+
+    /**
+     * Grab an item and store it in inventory
+     * 
+     * @param item the item to grab
+     */
+    public void grab(String item) {
+        this.inventory.add(item);
+        this.recordAction("grab");
+        this.recordArgs(item);
+        System.out.println("You have grabbed a " + item);
+    }
+
+    /**
+     * Drop an item and remove it from inventory. Checks to see if the item is in
+     * inventory first
+     * 
+     * @param item the item to drop
+     * @return the text that the computer will print to the user after dropping.
+     */
+    public String drop(String item) {
+        // TO DO: use Item() class instead of String().
+        String speech = "";
+        if (this.inventory.contains(item)) {
+            this.inventory.remove(item);
+            speech = "You have dropped " + item + ".";
+            this.recordAction("drop");
+            this.recordArgs(item);
+        } else {
+            speech = item + " is not in your inventory, you cannot drop it.";
+        }
+        System.out.println(speech);
+        return (speech);
+    }
+
+    /**
+     * Examine an item.
+     * 
+     * @param item the item to examine
+     */
+    public void examine(String item) {
+        String itemDescription = "This is a placeholder description for when I implement this skeleton code in my final project. In reality, this will be an attribute of the item containing a string description.";
+        System.out.println(itemDescription);
+    }
+
+    /**
+     * OVERLOADED TO USE MY DEFINED CLASS: Examine an item.
+     * 
+     * @param item the item to examine
+     */
+    public void examine(Item item) {
+        System.out.println(item.longDesc);
+    }
+
+    /**
+     * Use an item.
+     * 
+     * @param item the item to use
+     */
+    public void use(String item) {
+        String result = "This is a placeholder textual result for when I implement this skeleton code in my final project.";
+        System.out.println(result);
+    }
+
+    /**
+     * OVERLOADED TO USE MY DEFINED CLASS:Use an item.
+     * 
+     * @param item the item to use
+     */
+    public void use(Item item) {
+        System.out.println(item.use());
+    }
+
+    /**
+     * Change the currentPlace.
+     * 
+     * @param item the direction to walk
+     */
+    public boolean walk(String direction) {
+        boolean validDirection = this.currentPlace.connections.containsKey(direction);
+        Place newPlace = this.currentPlace.moveDirection(direction);
+        this.currentPlace = newPlace;
+        this.exhaust(1); // walking is somewhat tiring
+        if (validDirection) {
+            this.recordAction("walk");
+            this.recordArgs(direction);
+        }
+        System.out.println("Your current location is: " + this.currentPlace.name);
+        return (validDirection);
+    }
+
+    /**
+     * Change the currentPlace via teleportation (for after the finding of a magical
+     * item).
+     * 
+     * @param x the x coordinate of the place to teleport to
+     * @param y the y coordinate of the place to teleport to
+     */
+    public boolean fly(int x, int y) {
+        boolean success = false;
+        for (Place place : map) {
+            if (place.x == x && place.y == y) {
+                success = true;
+                break;
+            }
+        }
+        this.exhaust(10); // flying is very tiring
+        return (success);
+    }
+
+    /**
+     * Shrink the player down in order to meet certain exploration requirements
+     * (small spaces, passageways, etc)
+     * 
+     * @return the new player's size for printing if desired.
+     */
+    public Number shrink() {
+        if (this.playerSize == 1) { // enforce minimum size
+            throw new RuntimeException();
+        }
+        this.playerSize -= 1;
+        this.exhaust(10); // shrinking is very tiring
+        this.recordAction("shrink");
+        this.recordArgs("");
+        return (this.playerSize);
+
+    }
+
+    /**
+     * Grow player in order to meet certain exploration requirements (large jumps,
+     * wall climbing, etc)
+     * 
+     * @return the new player's size for printing if desired.
+     */
+    public Number grow() {
+        if (this.playerSize > 4) { // enforce maximum size
+            throw new RuntimeException();
+        }
+        this.playerSize += 1;
+        this.exhaust(10); // growing in size is very tiring
+        this.recordAction("grow");
+        this.recordArgs("");
+        return (this.playerSize);
+    }
+
+    /**
+     * Exhausts the player, called after certain taxing actions.
+     * 
+     * @param amt the amount to exhaust the player.
+     */
+    private void exhaust(int amt) {
+        this.playerEnergy -= amt;
+        if (this.playerEnergy < 5) {
+        System.out.println("You are so tired that you can't stay awake. Sweet dreams!");
+        this.rest();
+        } else if (this.playerEnergy < 10) {
+            System.out.println("Listen, you really need to sleep now.");
+        } else if (this.playerEnergy < 20) {
+            System.out.println("You are getting tired, please sleep.");
+        }
+    }
+
+    /**
+     * Rests the player (representative of sleeping), resetting their energy level
+     * to full.
+     */
+    public void rest() {
+        this.playerEnergy = 100;
+    }
+
+    /**
+     * Undoes the last "undoable" action by this player. An undoable action
+     * includes: grabbing / dropping an item, walking, shrinking, and growing. Any
+     * other action is something that does not make sense to undo. Also adds energy
+     * back to the player if exhaust() was called.
+     * 
+     * @param
+     */
+    public void undo() {
+        String lastAction = this.actions.get(this.actions.size() - 1);
+        String lastArg = this.actionArgs.get(this.actions.size() - 1);
+        if (lastAction == "grab") {
+            this.drop(lastArg);
+            // Remove the last 2 (because we called an undoable action in undoing the first
+            // action) elements from the actions(Args) lists
+            this.actions.removeLast();
+            this.actions.removeLast();
+            this.actionArgs.removeLast();
+            this.actionArgs.removeLast();
+        } else if (lastAction == "drop") {
+            this.grab(lastArg);
+            this.actions.removeLast();
+            this.actions.removeLast();
+            this.actionArgs.removeLast();
+            this.actionArgs.removeLast();
+        } else if (lastAction == "walk") {
+            if (lastArg == "north") {
+                this.walk("south");
+            } else if (lastArg == "south") {
+                this.walk("north");
+            } else if (lastArg == "east") {
+                this.walk("west");
+            } else if (lastArg == "west") {
+                this.walk("east");
+            }
+            this.actions.removeLast();
+            this.actions.removeLast();
+            this.actionArgs.removeLast();
+            this.actionArgs.removeLast();
+            this.playerEnergy += 2; // add 2 energy back to reflect the undoing
+        } else if (lastAction == "shrink") {
+            this.grow();
+            this.actions.removeLast();
+            this.actions.removeLast();
+            this.actionArgs.removeLast();
+            this.actionArgs.removeLast();
+            this.playerEnergy += 10; // add 10 energy back to reflect the undoing
+        } else if (lastAction == "grow") {
+            this.shrink();
+            this.actions.removeLast();
+            this.actions.removeLast();
+            this.actionArgs.removeLast();
+            this.actionArgs.removeLast();
+            this.playerEnergy += 10; // add 10 energy back to reflect the undoing
+        }
+    }
+
+    public void lookAround() {
+        System.out.println(this.currentPlace);
+    }
+    /**
+     * Records an action to the list of precent recorded actions. If the list is 10
+     * long, moves every item in the list over one and adds the action in the last
+     * place, thus deleting the 10th-most-recent entry
+     * 
+     * @param args  the string name of the function that was called 
+     */
+    private void recordAction(String action) {
+        if (this.actions.size() < 10) {
+            this.actions.add(action);
+        } else {
+            // traverse the list and move elements to right
+            for (int j = 0; j < 10; j++) {
+                this.actions.set(j, this.actions.get(j + 1));
+            }
+            this.actions.add(action);
+        }
+    }
+
+    /**
+     * Records the argument of the most recent action to the list of precent recorded agrs. If the list is 10
+     * long, moves every item in the list over one and adds the agr in the last,
+     * place, thus deleting the 10th-most-recent entry. If no arg was used in the function call, "" is filled in. 
+     * 
+     * @param args  the string name of the arg to the function that was called 
+     */
+    private void recordArgs(String args) {
+        if (this.actionArgs.size() < 10) {
+            this.actionArgs.add(args);
+        } else {
+            // traverse the list and move elements to left
+            for (int j = 0; j < 10; j++) {
+                this.actionArgs.set(j, this.actionArgs.get(j + 1));
+            }
+            this.actionArgs.add(args);
+        }
+    }
+
+    /**
+     * Prints the 10 most recently called "undoable" actions in least recent - most recent order. 
+     */
+    public void printActions() {
+        for (int i = 0; i <= this.actions.size() - 1; i++) {
+            System.out.println("Action " + (i + 1) + ": " +
+                    this.actions.get(i) + "(" + this.actionArgs.get(i) + ")");
+        }
+    }
+
+    public static void main(String[] args) {
+        Place livingRm = new Place(
+                new ArrayList<Item>(
+                        Arrays.asList(new Item("Coffee table"), new Item("Chair"),
+                                new Item("Chair"), new Item("Chair"), new Item("Rug"),
+                                new Item("Fireplace"))),
+                "Living Room",
+                "A living room with three chairs, a rug, and a table.",
+                "A cozy living room that contains three faded red checkered chairs, a worn beige rug, and an oak coffee table, still stained with the remnants of someone's bad morning. A fireplace lies unlit against the east wall.");
+        Place kitchenRm = new Place(
+                new ArrayList<Item>(
+                        Arrays.asList(new Item("Stove"), new Item("Sink"),
+                                new Item("Pantry Cabinet"))),
+                "Kitchen",
+                "A kitchen with a stove, sink, and pantry cabinet",
+                "A tile-floored kitchen. Against the west wall is a stove. The north wall is taken up by a large sink basin and an empty pantry cabinet");
+        livingRm.addConnection("south", kitchenRm);
+        Player me = new Player(livingRm, new ArrayList<Place>(Arrays.asList(kitchenRm, livingRm)));
+        me.lookAround();
+        me.walk("north");
+        me.walk("south");
+        me.grab("coffee");
+        me.grab("cat!");
+        me.drop("cat!");
+        me.printActions();
+        me.undo();
+        me.printActions();
+    }
+
+}
--- reflection.md
+++ reflection.md
@@ -1,5 +1,25 @@
 Use this file to record your reflection on this assignment.
 
 - Tell me about your class! What does it represent?
+
+    This class represents a player character in a D&D - style closed-world exploration / puzzle solving game. Within this class a player can move around, examine / take / drop objects, and observe their surroundings. They automatically tire when certain actions are taken (walk, grow, shrink, fly), and can be forced to rest when they get too tired (or can take a nap of their own volition). 
+
+    Along with the Player() class, I also made the Place() and Item() classes. These are both very bare-bones classes becuase they are meant to be the parent classes for specific child classes in the future (i.e. a Boat() that is an extension of Item() or a BedRoom() that is an extension of Place()). 
 - What additional methods (if any) did you implement alongside those listed in the interface?
+
+To start, here are the things I didn't do, even though one could reasonably consider then within the scope of this program:
+* I didn't make the grab() and drop() methods take in Item()s even though that makes the most sense, because I decided that was a scaling of this program that could wait until the next stage of building. 
+Make the exploration stage that requires use of the grow(), shrink(), or fly() methods. They are location-specific, and my goal was to just make the skeleton of the program, not design the implementation. 
+
+Ok, now for the additional methods! 
+* An overloaded examine() that takes an Item as the argument instead of a string. This is so that in a more final implementation of this program, I can fetch descriptions from within the objects they belong to. 
+* An overloaded use() that takes an Item. This connects to the Item.use() function, which is specific to each Item subclass. 
+* lookAround(): prints the description of the current place for if the player needs reminding of where they are
+* recordAction() and recordArgs(): For recordable actions (grab(), drop(), walk(), shirnk(), grow()) adds the action/corresponding argument to a list of 10 most recent actions. These are the actions that can be undone. If more than 10 actions are recorded, these functions boot the first item (aka least recent) out of the list, move everything over one slot, and add the new action to the end of the list (most recent action). 
+* printActions(): prints the list of the last 10 recordable actions
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+Worked: Setting up support classes was less complicated than I thought it would be! Big win there
+Didn't work: I attempted to make the Player.undo() function literally destroy the player instance it was called from. AFter a decent amount of research + office hours, I have realized that is not possible/advisable to do. 
+Advice: Start by planning out what each method will do before writing any of the code. I started by writing the code for the methods whose implementation was easiest to think of, then ended up getting a little stumped on how to apply the trickier methods to what I had already built. It ended up working out in the end, but was slightly more complicated than it probably needed to be. 
